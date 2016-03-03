3005714e54ede4975f7ba6254a26ee78f7eb03fd
Zeffer <zeffer5789@gmail.com>
2016-03-03 12:06:38 -0600

    Generates unique emails

---
+++ main.py
@@ -0,0 +1,27 @@
+import urllib, urllib2
+import random
+
+eSuffixes = [ "@gmail.com", "@hotmail.com", "@outlook.com", "@yahoo.com" ]
+
+def genUser():
+    int1 = random.randint(0, 2335)
+    int2 = random.randint(0, 2335)
+    data = open("words.txt")
+    
+    for i, line in enumerate(data):
+        if i == int1:
+            word1 = line
+        elif i == int2:
+            word2 = line
+
+    user = word1.capitalize() + word2.capitalize()
+
+    if random.randint(0, 999)%3 is not 0:
+        user += str(random.randint(0, 999))
+
+    user = user.replace("\n", "")
+
+    user += eSuffixes[random.randint(0, len(eSuffixes) - 1)]
+
+    return user
+
